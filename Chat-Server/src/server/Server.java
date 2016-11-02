3a113e9c10c344b7ade759a78ead0ae6986b9a3f
Kenneth White <kennival@hotmail.com>
2016-11-01 19:14:37 -0700

    Trying to undelete files

---
+++ Chat-Server/src/server/Server.java
@@ -0,0 +1,35 @@
+package server;
+
+/**
+ * Created by CW-Aspire on 11/1/2016.
+ */
+
+//imports
+import java.io.*;
+import java.net.*;
+import java.util.*;
+
+public class Server {
+    public static void main(String[] args)throws IOException{
+        ServerSocket listener = new ServerSocket(9090);     //creates a listening socket on port 9090 to wait for client connection
+        System.out.println("Server awaiting connection");
+        try{
+            while(true){
+                Socket socket = listener.accept();  //accept() waits until a client connects to the server on the given port.
+                System.out.println("Connection Success");
+                try{
+                    PrintWriter out = new PrintWriter(socket.getOutputStream(), true);  //this is the output to the client from the erver
+                    out.println("the current date is :" + new Date().toString());
+                }
+                finally {
+                    socket.close(); //make sure to always close sockets after use
+                    System.out.println("Connection Closed");
+                }
+            }
+        }
+        finally {
+            listener.close();       //make sure to always close sockets after use
+            System.out.println("Server Closed");
+        }
+    }
+}
