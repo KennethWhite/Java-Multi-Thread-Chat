c68a5f09b4ade771fc0a1542eac135da47489574
Cwhite534 <caseywhite534@gmail.com>
2016-11-01 20:24:15 -0700

    commit

--- Chat-Client/src/client/Client.java
+++ Chat-Client/src/client/Client.java
@@ -18,7 +18,8 @@ public class Client {
         BufferedReader input = new BufferedReader(new InputStreamReader(s.getInputStream()));       //creates a buffered reader for input from server
         String ans = input.readLine();                      //gets input
         System.out.println("Data from server received");
-        JOptionPane.showMessageDialog(null, ans);           //shows the imput recieved from server fsdfs
+        JOptionPane.showMessageDialog(null, ans);           //shows the imput recieved from server fsdfssadad
+        //asda
         s.close();
         System.exit(0);
     }
