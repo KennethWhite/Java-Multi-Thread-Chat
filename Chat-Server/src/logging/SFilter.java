a67d9b7e0d32ed085d46c76a8c3bb3c26f8c29ef
Kenneth White <kennival@hotmail.com>
2016-11-15 23:32:54 -0800

    Redesigned logging package (#45)
    Created a single filter class.
    Overloaded setupLogger method to easily create custom Loggers.

---
+++ Chat-Server/src/logging/SFilter.java
@@ -0,0 +1,44 @@
+package logging;
+
+import java.util.logging.Filter;
+import java.util.logging.Level;
+import java.util.logging.LogRecord;
+import java.util.logging.Logger;
+
+/**
+ * Created by Kenny on 11/15/2016.
+ */
+public class SFilter implements Filter {
+
+
+        private Level level;
+
+
+        SFilter(Level var){
+            if(var == null){
+                throw new IllegalArgumentException("Level param null on call to CTOR");
+            }
+            this.level = var;
+        }
+
+        public void updateFilter(Level in){
+            if(in == null){
+                throw new IllegalArgumentException("Level param null on call to updateFilter");
+            }
+
+            this.level = in;
+        }
+
+        @Override
+        public boolean isLoggable(LogRecord record) {
+            if(record.getLevel().equals(this.level)){
+                //returns true if the level is of the highest priority(SEVERE)
+                return true;
+            }
+            else {
+                return false;
+            }
+        }
+    }
+
+
