7834f3db997eed2bdc983be72fe4b9340c5e8dc3
Cwhite534 <caseywhite534@gmail.com>
2017-05-02 22:29:10 -0700

    Client Gui update
    added audio send menu
    changed message area to Text Flow

---
+++ Chat-Client/src/client/display/SendAudioSceneController.java
@@ -0,0 +1,81 @@
+package client.display;
+
+
+import client.Client;
+
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.ProgressBar;
+import javafx.scene.control.TextArea;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextFlow;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class SendAudioSceneController implements Initializable{
+
+    private Client client;
+    private TextFlow messageArea;
+    private ProgressBar progressBar;
+
+    public void initialize(URL location, ResourceBundle resources) {
+        client = Client.getClient();
+    }
+
+    /**
+     * This method serves as a way to initialize an instance of this controller, and pass objects from calling class
+     * @param root the scene to display to user
+     * @param textField the chat window text field
+     */
+    public  void setup(Parent root, TextFlow textField){
+        this.messageArea = textField;
+        Stage audioStage = new Stage();
+        audioStage.initModality(Modality.WINDOW_MODAL);
+        audioStage.initStyle(StageStyle.DECORATED);
+        audioStage.setTitle("Send Audio");
+        audioStage.setScene(new Scene(root));
+        audioStage.show();
+    }
+
+
+
+    /**
+     * records audio
+     */
+    public void recordBtnHandler() {
+        Text nt = new Text("Cannot record yet!\n");
+        nt.setFill(Color.LIME);
+        nt.setFont(Font.font("Monaco", 15));
+        messageArea.getChildren().add(nt);
+        //client.voiceLine(this);
+    }
+
+    /**
+     * plays the recorded audio
+     */
+    public void listenBtnHandler() {
+        Text nt = new Text("Cannot listen yet!\n");
+        nt.setFill(Color.BLUE);
+        nt.setFont(Font.font("Comic Sans MS", 20));
+        messageArea.getChildren().add(nt);
+
+    }
+
+    /**
+     * sends the audio to server
+     */
+    public void sendBtnHandler() {
+        Text nt = new Text("Cannot send yet!\n");
+        nt.setFill(Color.RED);
+        nt.setFont(Font.font("Verdana", 25));
+        messageArea.getChildren().add(nt);
+        //client.sendLine(this);
+    }
+
+}
